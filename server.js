2f8b3688161bd583217cd4f1d8812c613cb5ecbf
castillo098 <losvideojuegos7268098@gmail.com>
2020-09-24 23:40:49 -0500

    modificacion en el puerto del server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express()
 app.use(express.static(__dirname + '/public'))
 
-
+var port = process.env.PORT || 3000
 var server = app.listen(2000, () => {
   console.log("Servidor listo en 2000")
 })
